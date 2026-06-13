cae178767051f38adb80820e036e57514503cb91
Davronbek Atadjanov <atadjanovdavronbek863@gmail.com>
2026-06-13 17:58:53 +0500

    Fix sync DB query in async CORS handler using sync_to_async

--- src/apps/business/handlers.py
+++ src/apps/business/handlers.py
@@ -1,13 +1,24 @@
 import base64
 from urllib.parse import urlparse
+from asgiref.sync import sync_to_async, async_to_sync
 from corsheaders.signals import check_request_enabled
 from apps.business.models import Business
+
 
 def encode_domain(domain):
     base64_encoded_domain = base64.b64encode(domain.encode("utf-8")).decode("utf-8")
     www_domain = "www." + domain
     base64_encoded_www_domain = base64.b64encode(www_domain.encode("utf-8")).decode("utf-8")
     return base64_encoded_domain, base64_encoded_www_domain
+
+
+def _check_business(encoded_domain, encoded_www_domain):
+    businesses = Business.objects.filter(hash_id__in=[encoded_domain, encoded_www_domain])
+    for business in businesses:
+        if business.plan:
+            return True
+    return False
+
 
 def cors_allow_mysites(sender, request, **kwargs):
     origin = request.headers.get("origin")
@@ -21,12 +32,7 @@ def cors_allow_mysites(sender, request, **kwargs):
         domain = domain[4:]
 
     encoded_domain, encoded_www_domain = encode_domain(domain)
-    businesses = Business.objects.filter(hash_id__in=[encoded_domain, encoded_www_domain])
+    return async_to_sync(sync_to_async(_check_business))(encoded_domain, encoded_www_domain)
 
-    for business in businesses:
-        if business.plan:
-            return True
-
-    return False
 
 check_request_enabled.connect(cors_allow_mysites)
